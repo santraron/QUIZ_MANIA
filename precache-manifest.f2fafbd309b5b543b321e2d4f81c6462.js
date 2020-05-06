ddbe890702cc53659980de9469aefa21078026a9
aritra363 <aritrapaul363@gmail.com>
2020-05-06 12:11:39 +0530

    Updates

---
+++ precache-manifest.f2fafbd309b5b543b321e2d4f81c6462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df53d6865a5cd2b7e045e4f4fc95b35",
+    "url": "/QUIZ_MANIA/index.html"
+  },
+  {
+    "revision": "8a94e3609a50d10a6ef0",
+    "url": "/QUIZ_MANIA/static/css/main.240521f9.chunk.css"
+  },
+  {
+    "revision": "9558c68fe60f6aea806a",
+    "url": "/QUIZ_MANIA/static/js/2.fe2f12a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QUIZ_MANIA/static/js/2.fe2f12a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a94e3609a50d10a6ef0",
+    "url": "/QUIZ_MANIA/static/js/main.b3bbf304.chunk.js"
+  },
+  {
+    "revision": "4a2c0489dd79aec0d8c5",
+    "url": "/QUIZ_MANIA/static/js/runtime-main.72933c47.js"
+  }
+]);
